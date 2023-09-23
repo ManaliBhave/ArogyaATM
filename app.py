d19d492b18f09931d44c6eb97c4650d667d71e5c
Aaman <aaman.bhowmick21@gmail.com>
2023-09-23 23:31:11 +0530

    Skin part completed

--- app.py
+++ app.py
@@ -82,53 +82,212 @@ def audio_lang():
     
     return redirect("/question/language")
     
-@app.route("/question/<string:quest>/<string:lang>/<int:ans>", methods=['GET', 'POST'])    
+@app.route("/question/<string:quest>/<string:lang>/<string:ans>", methods=['GET', 'POST'])    
 def question(quest, lang, ans):
-    if lang == 'english':
-        if quest == 'fever':
-            return render_template("fever_eng.html")
-        elif quest == 'cold':
-            answers.append(ans)
-            print(answers)
-            return render_template("cold_eng.html")
+    if quest == 'flu':
+            return render_template("flu.html", lang = lang)
+    elif quest == 'resp':
+        answers.append(int(ans))
+        return render_template("resp.html", lang = lang)
+    
+    # ------------------------------------------------------------
+    elif quest == 'skin':
+        answers.append(int(ans))
+        return render_template("skin.html", lang = lang)
+    elif quest == 'itch':
+        answers.append(int(ans))
+        return render_template("skin/1_skin_itch.html", lang = lang)
+    elif quest == 'rash':
+        answers.append(int(ans))
+        return render_template("skin/2_skin_rash.html", lang = lang)
+    elif quest == 'erupt':
+        answers.append(int(ans))
+        return render_template("skin/3_skin_erupt.html", lang = lang)
+    elif quest == 'spots':
+        answers.append(int(ans))
+        return render_template("skin/4_skin_spots.html", lang = lang)
+    
+    # ------------------------------------------------------------
+    elif quest == 'digest':
+        if len(ans) > 1:
+            for i in range(0, len(str(ans))):
+                answers.append(0)
         else:
-            pass
-    else:
-        if quest == 'fever':
-            return render_template("fever_hin.html")
-        elif quest == 'cold':
-            answers.append(ans)
-            print(answers)
-            return render_template("cold_hin.html")
-        else:
-            pass
+            answers.append(int(ans))
+        return render_template("digest.html", lang = lang)
+    elif quest == 'kidney':
+        answers.append(int(ans))
+        return render_template("kidney.html", lang = lang)
+    elif quest == 'bone':
+        answers.append(int(ans))
+        return render_template("bone.html", lang = lang)
+    elif quest == 'weight':
+        answers.append(int(ans))
+        return render_template("weight.html", lang = lang)
+    elif quest == 'mental':
+        answers.append(int(ans))
+        return render_template("mental.html", lang = lang)
+    elif quest == 'heart':
+        answers.append(int(ans))
+        return render_template("heart.html", lang = lang)
+    elif quest == 'eyes':
+        answers.append(int(ans))
+        return render_template("eyes.html", lang = lang)
 
 @app.route("/question/audio/<string:quest>/<string:lang>", methods=['POST'])
 def question_lang(quest, lang):
     if lang == 'eng_audio':
-        if quest == 'fever':
-            wlcm_sound_eng = "./static/audio/fever_eng.wav"
+        if quest == 'flu':
+            wlcm_sound_eng = "./static/audio/flu_like/english/flu_eng.wav"
             wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
             play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
             play_obj_wlcm_sound_eng.wait_done()
-        elif quest == 'cold':
-            wlcm_sound_eng = "./static/audio/cold_eng.wav"
+        elif quest == 'resp':
+            wlcm_sound_eng = "./static/audio/resp_like/english/resp_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+            
+        # ---------------------------------------------------------------------------
+        elif quest == 'skin':
+            wlcm_sound_eng = "./static/audio/skin_like/english/skin_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'itch':
+            wlcm_sound_eng = "./static/audio/skin_like/english/itch_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'rash':
+            wlcm_sound_eng = "./static/audio/skin_like/english/rash_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'erupt':
+            wlcm_sound_eng = "./static/audio/skin_like/english/erupt_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'spots':
+            wlcm_sound_eng = "./static/audio/skin_like/english/spots_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+            
+        # ---------------------------------------------------------------------------
+        elif quest == 'digest':
+            wlcm_sound_eng = "./static/audio/digest_like/english/digest_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'kidney':
+            wlcm_sound_eng = "./static/audio/kidney_like/english/kidney_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'bone':
+            wlcm_sound_eng = "./static/audio/bone_like/english/bone_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'weight':
+            wlcm_sound_eng = "./static/audio/weight_like/english/weight_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'mental':
+            wlcm_sound_eng = "./static/audio/mental_like/english/mental_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'heart':
+            wlcm_sound_eng = "./static/audio/heart_like/english/heart_eng.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'eyes':
+            wlcm_sound_eng = "./static/audio/eyes_like/english/eyes_eng.wav"
             wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
             play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
             play_obj_wlcm_sound_eng.wait_done()
         else:
             pass
     else:
-        if quest == 'fever':
-            wlcm_sound_hin = "./static/audio/fever_hin.wav"
+        if quest == 'flu':
+            wlcm_sound_hin = "./static/audio/flu_like/hindi/flu_hin.wav"
             wave_obj_wlcm_sound_hin = sa.WaveObject.from_wave_file(wlcm_sound_hin)
             play_obj_wlcm_sound_hin = wave_obj_wlcm_sound_hin.play()
             play_obj_wlcm_sound_hin.wait_done()
-        elif quest == 'cold':
-            wlcm_sound_hin = "./static/audio/cold_hin.wav"
+        elif quest == 'resp':
+            wlcm_sound_hin = "./static/audio/resp_like/hindi/resp_hin.wav"
             wave_obj_wlcm_sound_hin = sa.WaveObject.from_wave_file(wlcm_sound_hin)
             play_obj_wlcm_sound_hin = wave_obj_wlcm_sound_hin.play()
             play_obj_wlcm_sound_hin.wait_done()
+            
+        # ----------------------------------------------------------------------------
+        elif quest == 'skin':
+            wlcm_sound_eng = "./static/audio/skin_like/hindi/skin_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'itch':
+            wlcm_sound_eng = "./static/audio/skin_like/hindi/itch_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'rash':
+            wlcm_sound_eng = "./static/audio/skin_like/hindi/rash_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'erupt':
+            wlcm_sound_eng = "./static/audio/skin_like/hindi/erupt_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'spots':
+            wlcm_sound_eng = "./static/audio/skin_like/hindi/spots_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        
+        # ----------------------------------------------------------------------------
+        elif quest == 'digest':
+            wlcm_sound_eng = "./static/audio/digest_like/hindi/digest_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'kidney':
+            wlcm_sound_eng = "./static/audio/kidney_like/hindi/kidney_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'bone':
+            wlcm_sound_eng = "./static/audio/bone_like/hindi/bone_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'weight':
+            wlcm_sound_eng = "./static/audio/weight_like/hindi/weight_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'mental':
+            wlcm_sound_eng = "./static/audio/mental_like/hindi/mental_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'heart':
+            wlcm_sound_eng = "./static/audio/heart_like/hindi/heart_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
+        elif quest == 'eyes':
+            wlcm_sound_eng = "./static/audio/eyes_like/hindi/eyes_hin.wav"
+            wave_obj_wlcm_sound_eng = sa.WaveObject.from_wave_file(wlcm_sound_eng)
+            play_obj_wlcm_sound_eng = wave_obj_wlcm_sound_eng.play()
+            play_obj_wlcm_sound_eng.wait_done()
         else:
             pass
     
